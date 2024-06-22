d599fbebe7f2ded6eb72bdc5b7e52a20217aa6a1
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-06-22 17:20:49 +0900

    2024

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -9,13 +9,13 @@ const MenuPage = () => {
     const [coffeeData, setCoffeeData] = useState([]);
     const [error, setError] = useState(null);
     const [visibleLinks, setVisibleLinks] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchCoffeeData = async () => {
             try {
                 const today = new Date().toISOString().split('T')[0];
-                const response = await fetch(`https://raw.githubusercontent.com/gnlgk/coffee-menu/main/${coffeeId}/menu${coffeeId}_2024-05-29.json`);
-
+                const response = await fetch(`https://raw.githubusercontent.com/gnlgk/menu-coffee/main/${coffeeId}/menu${coffeeId}_${today}.json`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
@@ -23,6 +23,8 @@ const MenuPage = () => {
                 setCoffeeData(result);
             } catch (error) {
                 setError(error);
+            } finally {
+                setLoading(false);
             }
         };
         fetchCoffeeData();
@@ -35,6 +37,10 @@ const MenuPage = () => {
         }));
     };
 
+    if (loading) {
+        return <div className="loading">메뉴를 불러오는 중입니다...</div>;
+    }
+
     if (error) {
         return <div>채널 정보를 불러오는 중에 에러가 발생했습니다: {error.message}</div>;
     }
@@ -46,34 +52,39 @@ const MenuPage = () => {
                     coffeeData.map((coffee, index) => (
                         <div className='coffee__Box' key={index}>
                             <div className="list__div">
-                                <img className='img' src={coffee.imageURL} alt={coffee.title} />
-                                <h3 className='title'>{coffee.title}</h3>
-                                <div className="tooltip">
-                                    <h4>{coffee.title}</h4>
-                                    <div className='titleeng'>{coffee.titleE}</div>
-                                    <div className='desc'>{coffee.desction}</div>
-                                    {coffee.information && (
-                                        <div className='info'><strong>영양성분</strong>
-                                            {Object.entries(coffee.information).map(([key, value]) => (
-                                                <div key={key}>
-                                                    {key} : {value}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )}
-                                    {visibleLinks[index] ? (
-                                        <a href={coffee.address} target="_blank" rel="noopener noreferrer" className='address'>{coffee.address}</a>
-                                    ) : (
-                                        <button onClick={() => showLink(index)} className="button-toggle">
-                                            더 보기
-                                        </button>
-                                    )}
+                                <div className="img"
+                                 style={{ backgroundImage: `url(${coffee.imageURL})` }}
+                                ></div>
+                                <div className="content">
+                                    <h3 className='title'>{coffee.title}</h3>
+                                    <div className="tooltip">
+                                        <h4>{coffee.title}</h4>
+                                        <div className='titleeng'>{coffee.titleE}</div>
+                                        <div className='desc'>{coffee.description}</div>
+                                        {coffee.information && (
+                                            <div className='info'>
+                                                <strong>영양성분</strong>
+                                                {Object.entries(coffee.information).map(([key, value]) => (
+                                                    <div key={key}>
+                                                        {key} : {value}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        )}
+                                        {visibleLinks[index] ? (
+                                            <a href={coffee.address} target="_blank" rel="noopener noreferrer" className='address'>{coffee.address}</a>
+                                        ) : (
+                                            <button onClick={() => showLink(index)} className="button-toggle">
+                                                더 보기
+                                            </button>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     ))
                 ) : (
-                    <div>메뉴를 불러오는 중입니다...</div>
+                    <div>메뉴가 없습니다.</div>
                 )}
             </div>
         </Main>
